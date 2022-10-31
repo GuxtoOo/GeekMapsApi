e5f84b09d031b1ae095e81374bcb01d4d073e3d8
augusto santos <augustomanoel527@gmail.com>
2022-10-31 10:51:31 -0300

    ✨ Add Dependencies Injections✨

---
+++ GeekMapsApi/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,47 @@
+﻿using GeekMapsApi.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace GeekMapsApi.Configurations;
+
+public static class DependencyInjectionConfig
+{
+    public static IServiceCollection AddDependencyInjectionConfig(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DBConnectionString");
+
+        //Config Services
+        services.AddDbContext<GeekMapsApiDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+            options.EnableSensitiveDataLogging(true);
+        });
+
+        var key = Encoding.ASCII.GetBytes(JwtSettings.Key);
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+
+        services.AddCors();
+
+        //Application
+
+        //Infrastructure
+
+        return services;
+    }
+}
