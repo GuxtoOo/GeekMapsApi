8dd2ad57e801a4c724fc0c1a2ebbc0e63cc9f2db
augusto santos <augustomanoel527@gmail.com>
2022-10-31 10:14:33 -0300

    ✨ Add Auto Mapper ✨

---
+++ GeekMapsApi/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace GeekMapsApi.Configurations;
+
+public static class AutoMapperConfig
+{
+    public static IServiceCollection AddAutoMapperConfig(this IServiceCollection services)
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+
+        services.AddAutoMapper(assembly);
+
+        return services;
+    }
+}
+
+//Maping Routes
+public class AutoMapperFastMapper : Profile
+{
+    public AutoMapperFastMapper()
+    {
+
+    }
+}
