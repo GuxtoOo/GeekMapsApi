396eff48242f5275779cc9b928baa087861089c7
augusto santos <augustomanoel527@gmail.com>
2022-10-31 10:52:10 -0300

    ✨ Add TokenJwt and Configuration ✨

---
+++ GeekMapsApi/Settings/JwtSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace GeekMapsApi.Settings
+{
+    public static class JwtSettings
+    {
+        public static string Key = ")H@McQfTjWnZr4u7w!z%C*F-JaNdRgUkXp2s5v8y/A?D(G+KbPeShVmYq3t6w9z$";
+    }
+}
