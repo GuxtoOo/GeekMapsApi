515c424e99b81e9a92326f8c7caf32664bfdd3d3
augusto santos <augustomanoel527@gmail.com>
2022-11-11 10:27:45 -0300

    Add: Connection with DB for Evento

--- GeekMapsApi.Infrastructure/Data/GeekMapsDbContext.cs
+++ GeekMapsApi.Infrastructure/Data/GeekMapsDbContext.cs
@@ -8,4 +8,5 @@ public class GeekMapsDbContext : DbContext
     public GeekMapsDbContext(DbContextOptions<GeekMapsDbContext> options) : base(options) { }    
 
     public DbSet<Administrador> Administrador { get;set; }
+    public DbSet<Evento> Evento { get; set; }
 }
